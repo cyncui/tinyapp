111a490e0556a1dc16b93510244280925edce31d
cyn <cuixintong@alumni.ubc.ca>
2021-06-07 15:37:30 -0700

    implemented code to save new shortURL to database

--- express_server.js
+++ express_server.js
@@ -42,16 +42,21 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars)
 });
 
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL
+  }
+  res.send("URL received!"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/u/:shortURL", (req, res) => {
   // console.log("longURL:", urlDatabase[req.params.shortURL]);
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("URL received!");         // Respond with 'Ok' (we will replace this)
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
